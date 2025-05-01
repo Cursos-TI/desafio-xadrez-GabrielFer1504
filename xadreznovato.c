90f03b90dd48421de85cd1160d07b23fb10d3760
GabrielFer1504 <gabrielbim6@gmail.com>
2025-05-01 12:10:28 +0000

    xadreznovato.c
    Olá, meu nome é Gabriel Fernando e estes são meus primeiros códigos desenlvolvendo o jogo de Xadrex.
    
    Neste nivel iniciante começamos usando as estruturas de decisão "For", "While", "Do-While", para declarar quais movimentos cada peça irá realizar.
    
    Utilizamos três peças do  Xadrex Bispo, Torre, Rainha, os movimentos são:
    
    Bispo: Cima Direita.
    Torre: Direita;
    Rainha: Esquerda;

---
+++ xadreznovato.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// Olá, Tudo Bem ?!
+// O objetivo deste programa é simular o movimento de três peças de xadrez, Torre, Bispo e Rainha usando estruturas de repetição.
+
+
+int main() {
+
+    int i;
+
+
+    printf("BEM-VINDO AO PROGRAMA DE SIMULAÇÃO DE MOVIMENTO DE PEÇAS DE XADREZ!!!\n");
+
+    printf("\n");
+
+    
+    // Movimento da torre 
+
+    printf("Movimento da torre: \n");
+    for (i = 0; i < 5; i++) 
+    {
+        printf("-> Direita.\n");
+    }
+
+    printf("\n");
+
+    // Movimento do Bispo
+
+    printf("MOvimento do Bispo: \n");
+    i = 0;
+    while (i < 5) 
+    {
+        printf("-> Cima Direita.\n");
+        i++;
+    }
+
+    printf("\n");
+
+    // Movimento da Rainha
+
+    printf("Movimento da Rainha: \n");
+    i = 0;
+    do {
+        printf("-> Esquerda.\n");
+        i++;
+    } while (i < 8);
+
+
+    return 0;
+}
